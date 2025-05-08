9c6b04d3b85530e40d357a78011584e8c1e66635
victoria A. <victoria.adetimehin@gmail.com>
2025-05-08 01:28:51 -0500

    Add pages directory with index

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+      <div style={{ textAlign: "center", padding: "2rem" }}>
+        <h1>🎂 Welcome to Baker Order App!</h1>
+        <p>Your bakery order tracker is live!</p>
+      </div>
+    );
+  }
